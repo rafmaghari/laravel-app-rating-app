ab2b8f43858dcd6e4479727dc8f527f34e052aa9
rafmaghari <29rafaelmaghari@gmail.com>
2021-05-08 12:14:49 +0800

    feat: Rating Api

--- app/Http/Controllers/Api/RatingController.php
+++ app/Http/Controllers/Api/RatingController.php
@@ -4,62 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Rating;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function store(Request $request): JsonResponse
     {
-        //
+        return response()->json(['message' => 'Rating has been add', 'data' => Rating::create($request->all())],201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update(Request $request, Rating $rating): JsonResponse
     {
-        //
+        return response()->json(['message' => 'Rating has been saved', 'data' => $rating->update($request->all())]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Rating  $rating
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Rating $rating)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Rating  $rating
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Rating $rating)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Rating  $rating
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Rating $rating)
-    {
-        //
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ItemCommentController;
 use App\Http\Controllers\Api\ItemController;
+use App\Http\Controllers\Api\RatingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/items', ItemController::class);
 Route::apiResource('/item-comments', ItemCommentController::class);
+Route::apiResource('/ratings', RatingController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
